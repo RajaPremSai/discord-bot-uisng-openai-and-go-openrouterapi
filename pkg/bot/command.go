808489f9e9cea3697f9b6c3f5c6345996204e307
RAJA PREM SAI MADDULA <123068364+RajaPremSai@users.noreply.github.com>
2025-11-05 15:10:12 +0530

    Defined bot pkg

--- pkg/bot/command.go
+++ pkg/bot/command.go
@@ -1,1 +1,35 @@
-//handler of commands
+// handler of commands
+package bot
+
+import (
+	discord "github.com/bwmarrin/discordgo"
+)
+
+type Handler interface {
+	HandlerCommand(ctx *Context)
+}
+
+type HandlerFunc func(ctx *Context)
+
+func (f HandlerFunc) HandleCommand(ctx *Context) { f(ctx) }
+
+type MessageHandler interface {
+	HandleMessageCommand(ctx *MessageContext)
+}
+
+type MessageHandlerFunc func(ctx *MessageContext)
+
+func (f MessageHandlerFunc) HandleMessageCommand(ctx *MessageContext) { f(ctx) }
+
+type Command struct {
+	Name                     string
+	Description              string
+	DMPermission             bool
+	DefaultMemberPermissions int64
+	Options                  []*discord.ApplicationCommandOption
+	Type                     discord.ApplicationCommandType
+	Handler                  Handler
+	Middlewares              []Handler
+	MessageHandler           MessageHandler
+	Subcommands              *Router
+}
