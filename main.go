ba275259cfee8152e637ed1fb95b5c41fe05e288
RAJA PREM SAI MADDULA <123068364+RajaPremSai@users.noreply.github.com>
2025-11-05 14:00:53 +0530

    Initailized func

--- main.go
+++ main.go
@@ -4,69 +4,70 @@ import (
 	"log"
 	"os"
 
+	"github.com/RajaPremSai/go-openai-dicord-bot/pkg/bot"
 	"github.com/sashabaranov/go-openai"
 	"github.com/stretchr/testify/assert/yaml"
 )
 
-type Config struct{
-	Discord struct{
-		Token string `yaml:"token"`
-		Guild string `yaml:"guild"`
-		RemoveCommands bool `yaml:"removeCommands"`
-	}`yaml:"discord"`
-	OpenAI struct{
-		APIKey string `yaml:"apiKey"`
+type Config struct {
+	Discord struct {
+		Token          string `yaml:"token"`
+		Guild          string `yaml:"guild"`
+		RemoveCommands bool   `yaml:"removeCommands"`
+	} `yaml:"discord"`
+	OpenAI struct {
+		APIKey           string   `yaml:"apiKey"`
 		CompletionModels []string `yaml:"completionModels"`
-	}`yaml:"openAI"`
+	} `yaml:"openAI"`
 }
 
-func (c *Config) ReadFromFile(file string)error{
-	data,err:=os.ReadFile(file)
-	if err!=nil{
+func (c *Config) ReadFromFile(file string) error {
+	data, err := os.ReadFile(file)
+	if err != nil {
 		return err
 	}
-	err=yaml.Unmarshal(data,c)
-	if err!=nil{
+	err = yaml.Unmarshal(data, c)
+	if err != nil {
 		return err
 	}
 	return err
 }
 
-func init(){
+func init() {
 	log.SetOutput(os.Stdout)
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
-var(
-	discordBot *bot.Bot
+var (
+	discordBot   *bot.Bot
 	openaiClient *openai.Client
 
-	gptMessageCache *gpt.MessagesCache
-	ignoredChannelCache=make(gpt.IgnoredChannelsCache)
+	gptMessageCache     *gpt.MessagesCache
+	ignoredChannelCache = make(gpt.IgnoredChannelsCache)
 )
 
-func main(){
-	config :=&Config{}
-	err:=config.ReadFromFile("credentials.yaml")
-	if err!=nil{
-		log.Fatalf("Error reading credentials.yaml: %v",err)
+func main() {
+	config := &Config{}
+	err := config.ReadFromFile("credentials.yaml")
+	if err != nil {
+		log.Fatalf("Error reading credentials.yaml: %v", err)
 	}
-	gptMessageCache,err = gpt.NewMessagesCache(constants.DiscordThreadsCacheSize)
-	if err!=nil{
-		log.Fatalf("Error initializing GPTMessageCache: %v",err)
+	gptMessageCache, err = gpt.NewMessagesCache(constants.DiscordThreadsCacheSize)
+	if err != nil {
+		log.Fatalf("Error initializing GPTMessageCache: %v", err)
 	}
-	discordBot,err := bot.NewBot(config.Discord.Token)
-	if err!=nil{
-		log.Fatalf("Inavalid parameters:%v",err)
+	discordBot, err := bot.NewBot(config.Discord.Token)
+	if err != nil {
+		log.Fatalf("Inavalid parameters:%v", err)
 	}
-	if config.OpenAI.APIKey !=""{
-		openaiClient=openai.NewClient(config.OpenAI.APIKey)
-		discordBot.Router.Register(commands.ChatCommand(&commands.ChatCommandParams{OpenAIClient:           openaiClient,
+	if config.OpenAI.APIKey != "" {
+		openaiClient = openai.NewClient(config.OpenAI.APIKey)
+		discordBot.Router.Register(commands.ChatCommand(&commands.ChatCommandParams{OpenAIClient: openaiClient,
 			OpenAICompletionModels: config.OpenAI.CompletionModels,
 			GPTMessagesCache:       gptMessagesCache,
-			IgnoredChannelsCache:   &ignoredChannelsCache,}))
+			IgnoredChannelsCache:   &ignoredChannelsCache}))
 		discordBot.Router.Register(commands.ImageCommand(openaiClient))
 	}
 	discordBot.Router.Register(commands.InfoCommand())
-	discordBot.Run(config.Discord.Guild,config.Discord.RemoveCommands)
+	discordBot.Run(config.Discord.Guild, config.Discord.RemoveCommands)
 }
--- pkg/bot/router.go
+++ pkg/bot/router.go
@@ -1,1 +1,63 @@
 //logic for registering the bot,handling messages, handling interactions
+
+package bot
+
+import (
+	discord "github.com/bwmarrin/discordgo"
+)
+
+type Router struct {
+	commands           map[string]*Command
+	registeredCommands []*discord.ApplicationCommand
+}
+
+func NewRouter(initial []*Command) (r *Router) {
+	r = &Router{commands: make(map[string]*Command, len(inital))}
+	for _, cmd := range initial {
+		r.Register(cmd)
+	}
+
+	return
+}
+
+func (r *Router) Register(cmd *Command) {
+	if _, ok := r.commands[cmd.Name]; !ok {
+		r.commands[cmd.Name] = cmd
+	}
+}
+
+func(r *Router)Get(name string)*Command{
+
+}
+
+func(r *Router)List()(list []*Command){
+
+}
+
+func(r *Router)Count()(c int){
+	
+}
+
+func( r* Router)getSubcommand(cmd *Command,opt *discord.ApplicationCommandInteractionDataOption,parent []Handler)(){
+
+}
+
+func ( r *Router)getMessageHandlers(cmd *Command)[]MessageHandler{
+
+}
+
+func (r *Router) HandleInteraction(s *discord.Session, i *discord.InteractionCreate) {
+
+}
+
+func (r *Router) HandleMessage(s *discord.Session, m *discord.MessageCreate) {
+
+}
+
+func (r *Router) Sync(s *discord.Session, guild string)(err error){
+
+}
+
+func(r *Router)ClearCommands(s *discord.Session,guild string)(Errors []error){
+
+}
