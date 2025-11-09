775f50d99f4264741b949dcbe3544ac94790a50f
RAJA PREM SAI MADDULA <123068364+RajaPremSai@users.noreply.github.com>
2025-11-09 23:43:02 +0530

    Defined util func

--- go.mod
+++ go.mod
@@ -7,9 +7,11 @@ require (
 	github.com/hashicorp/golang-lru/v2 v2.0.7
 	github.com/sashabaranov/go-openai v1.41.2
 	github.com/stretchr/testify v1.11.1
+	github.com/tiktoken-go/tokenizer v0.7.0
 )
 
 require (
+	github.com/dlclark/regexp2 v1.11.5 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
--- pkg/commands/gpt/utils.go
+++ pkg/commands/gpt/utils.go
@@ -1,2 +1,74 @@
 package gpt
 
+import (
+	"github.com/RajaPremSai/go-openai-dicord-bot/pkg/bot"
+	discord "github.com/bwmarrin/discordgo"
+	"github.com/sashabaranov/go-openai"
+)
+
+const (
+	gptPricePerPromptTokenGPT3Dot5Turbo0613     = 0.0000015
+	gptPricePerCompletionTokenGPT3Dot5Turbo0613 = 0.000002
+
+	gptPricePerPromptTokenGPT3Dot5Turbo16K0613     = 0.000003
+	gptPricePerCompletionTokenGPT3Dot5Turbo16K0613 = 0.000004
+
+	gptPricePerPromptTokenGPT40613     = 0.00003
+	gptPricePerCompletionTokenGPT40613 = 0.00006
+
+	gptPricePerPromptTokenGPT432K0613     = 0.00006
+	gptPricePerCompletionTokenGPT432K0613 = 0.00012
+)
+
+const (
+	gptTruncateLimitGPT3Dot5Turbo0301 = 3500
+	gptTruncateLimitGPT40314          = 6500
+	gptTruncateLimitGPT432K0314       = 30500
+)
+
+func shouldHandleMessageType(t discord.MessageType) bool {
+	return t == discord.MessageTypeDefault || t == discord.MessageTypeReply
+}
+
+type chatGPTResponse struct {
+	content string
+	usage   openai.Usage
+}
+
+func sendChatGPTRequest(client *openai.Client, cacheItem *MessagesCacheData) (*chatGPTResponse, error) {
+
+}
+
+func getUrlData(client *http.Client, url string) (string, error) {
+
+}
+
+func getContentOrURLData(client *http.Client, s string) (content string, err error) {
+
+}
+
+func parseInteractionReply(discordMessage *discord.Message) (prompt string, context string, model string, temperature *float32) {
+
+}
+
+func modelTruncateLimit(model string) *int {
+
+}
+
+func attachUsageInfo(s *discord.Session, m *discord.Message, usage openai.Usage, model string){
+
+}
+
+func generateCost(usage openai.Usage, model string) string {
+
+}
+
+func adjustMessageTokens(cacheItem *MessagesCacheData) {
+
+}
+
+func isCacheItemWithinTurncateLimit(cacheItem *MessagesCacheData) (ok bool, count int) {
+
+}
+
+func generateThreadTitleBasedOnInitialPrompt(ctx *bot.Context, client *openai.Client, threadID string, messages []openai.ChatCompletionChoice)
