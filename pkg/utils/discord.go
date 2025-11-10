a87ea0ba06acb8a449a24a750beed71526346762
RAJA PREM SAI MADDULA <123068364+RajaPremSai@users.noreply.github.com>
2025-11-10 20:33:53 +0530

    feat: discord msg handling

--- pkg/utils/discord.go
+++ pkg/utils/discord.go
@@ -1,1 +1,37 @@
 package utils
+
+import (
+	"log"
+
+	discord "github.com/bwmarrin/discordgo"
+)
+
+func ToggleDiscordThreadLock(s *discord.Session, channelID string, locked bool) {
+	_, err := s.ChannelEditComplex(channelID, &discord.ChannelEdit{
+		Locked: &locked,
+	})
+	if err != nil {
+		log.Printf("[CHID: %s] Failed to lock/unlock Thread with the error: %v\n", channelID, err)
+	}
+}
+
+func DiscordChannelMessageSend(s *discord.Session, channelID string, content string, messageReference *discord.MessageReference) (m *discord.Message, err error) {
+	if messageReference != nil {
+		m, err = s.ChannelMessageSendReply(channelID, content, messageReference)
+	} else {
+		m, err = s.ChannelMessageSend(channelID, content)
+	}
+	return
+}
+
+func DiscordChannelMessageEdit(s *discord.Session, messageID string, channelID string, content *string, embeds []*discord.MessageEmbed) error {
+	_, err := s.ChannelMessageEditComplex(
+		&discord.MessageEdit{
+			Content: content,
+			Embeds:  embeds,
+			ID:      messageID,
+			Channel: channelID,
+		},
+	)
+	return err
+}
